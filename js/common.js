fa0c90fe1c6f1fccd252c878597c4a8190a9a41a
KimDongGyu <ehdrn1205@gmail.com>
2023-05-12 02:10:30 +0900

    set common file(html/css/js)

---
+++ js/common.js
@@ -0,0 +1,18 @@
+const body = document.getElementsByTagName("body")[0];
+
+// header div 생성 및 body에 붙이기
+const header = document.createElement("div");
+header.id = "header";
+body.prepend(header);
+
+// footer div 생성 및 body에 붙이기
+const footer = document.createElement("div");
+footer.id = "footer";
+body.appendChild(footer);
+
+
+// header.html와 footer.html 들고오기
+$(document).ready(function(){
+    $("#header").load("./header.html");
+    $("#footer").load("./footer.html");
+});
